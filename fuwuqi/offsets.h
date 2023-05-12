a8c292aec3abcd3f4a2c661c4e9fe1390f589359
tea321000 <834914152@qq.com>
2023-05-12 23:12:41 +0800

    update s17

--- fuwuqi/offsets.h
+++ fuwuqi/offsets.h
@@ -5,27 +5,27 @@
 
 #if VERSION == STEAM
 
-#define OFFSET_ENTITYLIST 0x1d0fe58 //cl_entitylist
-#define OFFSET_LOCAL_ENT 0x20c1138 //LocalPlayer 0x20c21d0 + 0x8
-#define OFFSET_NAME_LIST 0xbffec80 //NameList
-#define OFFSET_THIRDPERSON 0x01cf49c0 + 0x6c //thirdperson_override + 0x6c
-#define OFFSET_TIMESCALE 0x016aa110 //host_timescale
+#define OFFSET_ENTITYLIST 0x1d87e88 //cl_entitylist
+#define OFFSET_LOCAL_ENT 0x02139488 //LocalPlayer 0x02139480 + 0x8
+#define OFFSET_NAME_LIST 0xbd27ba0 //NameList
+#define OFFSET_THIRDPERSON 0x01d17600 + 0x6c //thirdperson_override + 0x6c
+#define OFFSET_TIMESCALE 0x016ccb40 //host_timescale
 
 #define OFFSET_TEAM 0x044c //m_iTeamNum
 #define OFFSET_HEALTH 0x043c //m_iHealth
 #define OFFSET_SHIELD 0x0170 //m_shieldHealth
 #define OFFSET_MAXSHIELD 0x0174 //m_shieldHealthMax
-#define OFFSET_ARMORTYPE 0x4634 //armortype
+#define OFFSET_ARMORTYPE 0x464c //armortype
 #define OFFSET_NAME 0x0589 //m_iName
 #define OFFSET_SIGN_NAME 0x0580 //m_iSignifierName
 #define OFFSET_ABS_VELOCITY 0x0140 //m_vecAbsVelocity
-#define OFFSET_VISIBLE_TIME 0x1a78 //CPlayer!lastVisibleTime
-#define OFFSET_ZOOMING 0x1c51 //m_bZooming
-#define OFFSET_THIRDPERSON_SV 0x36c8 //m_thirdPersonShoulderView
-#define OFFSET_YAW 0x22b4 - 0x8 //m_currentFramePlayer.m_ammoPoolCount - 0x8
+#define OFFSET_VISIBLE_TIME 0x1a80 //CPlayer!lastVisibleTime
+#define OFFSET_ZOOMING 0x1c61 //m_bZooming
+#define OFFSET_THIRDPERSON_SV 0x36e0 //m_thirdPersonShoulderView
+#define OFFSET_YAW 0x22c4 - 0x8 //m_currentFramePlayer.m_ammoPoolCount - 0x8
 
 #define OFFSET_LIFE_STATE 0x0798 //m_lifeState, >0 = dead
-#define OFFSET_BLEED_OUT_STATE 0x2740 //m_bleedoutState, >0 = knocked
+#define OFFSET_BLEED_OUT_STATE 0x2750 //m_bleedoutState, >0 = knocked
 
 #define OFFSET_ORIGIN 0x014c //m_vecAbsOrigin - 3rd offset after the first int and vector
 
@@ -34,19 +34,19 @@
 #define OFFSET_STUDIOHDR 0x10f0 //CBaseAnimating!m_pStudioHdr
 
 
-#define OFFSET_AIMPUNCH 0x24b0 //m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
-#define OFFSET_CAMERAPOS 0x1f48 //CPlayer!camera_origin
-#define OFFSET_VIEWANGLES 0x25ac - 0x14 //m_ammoPoolCapacity - 0x14
+#define OFFSET_AIMPUNCH 0x24c0 //m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+#define OFFSET_CAMERAPOS 0x1f58 //CPlayer!camera_origin
+#define OFFSET_VIEWANGLES 0x25bc - 0x14 //m_ammoPoolCapacity - 0x14
 #define OFFSET_BREATH_ANGLES OFFSET_VIEWANGLES - 0x10
-#define OFFSET_OBSERVER_MODE 0x34d4 //m_iObserverMode
-#define OFFSET_OBSERVING_TARGET 0x34e0 //m_hObserverTarget
+#define OFFSET_OBSERVER_MODE 0x34ec //m_iObserverMode
+#define OFFSET_OBSERVING_TARGET 0x34f8 //m_hObserverTarget
 
-#define OFFSET_MATRIX 0x11a210 //ViewMatrix
-#define OFFSET_RENDER 0x786f390 //ViewRender
+#define OFFSET_MATRIX 0x11a350 //ViewMatrix
+#define OFFSET_RENDER 0x7400bd0 //ViewRender
 
-#define OFFSET_WEAPON 0x1a1c //m_latestPrimaryWeapons
-#define OFFSET_BULLET_SPEED 0x1f48 //CWeaponX!m_flProjectileSpeed
-#define OFFSET_BULLET_SCALE 0x1f50 //CWeaponX!m_flProjectileScale
+#define OFFSET_WEAPON 0x1a24 //m_latestPrimaryWeapons
+#define OFFSET_BULLET_SPEED 0x1A88 + 0x04c8 //CWeaponX!m_flProjectileSpeed
+#define OFFSET_BULLET_SCALE 0x1A88 + 0x04d0 //CWeaponX!m_flProjectileScale
 #define OFFSET_ZOOM_FOV 0x16c0 + 0xb8 //m_playerData + m_curZoomFOV
 #define OFFSET_AMMO 0x1644 //m_ammoInClip first offset 
 
@@ -54,7 +54,7 @@
 #define OFFSET_ITEM_ID 0x1628 // item id?
 #define OFFSET_MODELNAME 0x0030 // m_ModelName
 #define OFFSET_M_CUSTOMSCRIPTINT 0x1648 //m_customScriptInt
-#define OFFSET_MINIMAP_SCALE 0x4620 //m_minimapTargetZoomScale, float
+#define OFFSET_MINIMAP_SCALE 0x4638 //m_minimapTargetZoomScale, float
 
 #define OFFSET_GLOW_T1 0x262 //16256 = enabled, 0 = disabled
 #define OFFSET_GLOW_T2 0x2dc //1193322764 = enabled, 0 = disabled
@@ -62,7 +62,7 @@
 #define OFFSET_GLOW_THROUGH_WALLS 0x3d0 //2 = enabled, 5 = disabled
 #define OFFSET_SKIN			        0x0e54 //m_nSkin
 #define OFFSET_m_fFlags             0x0098 //player is in air
-#define OFFSET_IN_FORWARD           0x07871be0 //player is in forward
+#define OFFSET_IN_FORWARD           0x07403420 //player is in forward
 
 #elif VERSION == ORIGIN
 
